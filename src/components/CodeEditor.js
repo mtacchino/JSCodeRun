d406d891bfb06441e9460b86695d3ef8d9c970eb
Matt Tacchino <matt.tacchino@ovoenergy.com>
2017-09-07 23:09:24 +0200

    remove unused imports

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { TextInput, Text, StyleSheet } from 'react-native';
-import SyntaxHighlighter from '../react-native-syntax-highlighter/src/index';
-import { docco } from '../react-syntax-highlighter/src/styles';
 
 const styles = StyleSheet.create({
   keywordStyle: {
