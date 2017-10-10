4913790138161222e472853817a4ed21e12a952c
mtacchino <matt@tacchino.me>
2017-10-10 20:45:20 +0100

    Tests for running code

--- src/screens/MainScreen.test.js
+++ src/screens/MainScreen.test.js
@@ -13,4 +13,63 @@ describe('MainScreen', () => {
     ).toJSON();
     expect(rendered).toMatchSnapshot();
   });
+
+  describe('runCode', () => {
+    it('should produce an array of a single output when code is logged', () => {
+      const instance = renderer.create(
+        <MainScreen />
+      ).getInstance();
+      
+      instance.setState({
+        code: 'console.log("Test output")'
+      });
+      const expected = [
+        {
+          message: 'Test output',
+          status: 'OK'
+        }
+      ];
+      instance.runCode();
+      expect(instance.state.output).toEqual(expected);
+    });
+
+    it('should produce an array of a multiple outputs when code is logged', () => {
+      const instance = renderer.create(
+        <MainScreen />
+      ).getInstance();
+      
+      instance.setState({
+        code: 'console.log("Test output"); console.log("More output");'
+      });
+      const expected = [
+        {
+          message: 'Test output',
+          status: 'OK'
+        }, {
+          message: 'More output',
+          status: 'OK'
+        }
+      ];
+      instance.runCode();
+      expect(instance.state.output).toEqual(expected);
+    });
+    
+    it('should produce an array of a multiple error outputs when code errors', () => {
+      const instance = renderer.create(
+        <MainScreen />
+      ).getInstance();
+      
+      instance.setState({
+        code: 'badcode'
+      });
+      const expected = [
+        {
+          message: 'ReferenceError: badcode is not defined',
+          status: 'ERROR'
+        }
+      ];
+      instance.runCode();
+      expect(instance.state.output).toEqual(expected);
+    });
+  })
 })
